3b817d35197a4ebfc13d55842f708d5103a5b227
dantevvp <dantevvp@gmail.com>
2021-08-03 17:12:38 -0300

    Attempt to fix rubocop offenses

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -67,10 +67,13 @@ module Api
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(%i[
-        email name
-        last_name password
-        password_confirmation
-      ])
+                                   email
+                                   name
+                                   last_name
+                                   password
+                                   password_confirmation
+                                   ]
+                                  )
     end
   end
 end
