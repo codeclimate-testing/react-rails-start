78bbf1eaa00f3b0bef63eec6eb02c8067b8bf43f
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-04-21 11:29:09 -0300

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -39,7 +39,7 @@ module Api
     private
 
     def incorrect_user_error
-      { json: { errors: 'Usuario asda' }, status: :unauthorized }
+      { json: { errors: 'Usuario ' }, status: :unauthorized }
     end
 
     def comment_errors
