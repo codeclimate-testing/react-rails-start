fdedc39273ead89bd175bd2195f39e72433bed3b
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-04-21 15:54:10 -0300

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -39,7 +39,7 @@ module Api
     private
 
     def incorrect_user_error
-      { json: { errors: 'asdoija ' }, status: :unauthorized }
+      { json: { errors: 'asfsa ' }, status: :unauthorized }
     end
 
     def comment_errors
