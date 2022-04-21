fd48da4bb3b47632e5b09e44f96b8407a5f5b88a
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-04-21 11:55:33 -0300

    test

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -39,7 +39,7 @@ module Api
     private
 
     def incorrect_user_error
-      { json: { errors: 'Usuario se' }, status: :unauthorized }
+      { json: { errors: 'Usuarioe' }, status: :unauthorized }
     end
 
     def comment_errors
