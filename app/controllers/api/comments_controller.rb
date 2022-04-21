302fd9951a89cc34add38bf1f0ea5bd149a5ba4b
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-04-21 14:08:54 -0300

    anda la magia?

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -39,7 +39,7 @@ module Api
     private
 
     def incorrect_user_error
-      { json: { errors: 'asdada' }, status: :unauthorized }
+      { json: { errors: 'anda la magia?' }, status: :unauthorized }
     end
 
     def comment_errors
