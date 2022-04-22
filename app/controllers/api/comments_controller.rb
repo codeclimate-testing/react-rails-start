718d1c893bee8a5f8cf8fbe37c1706c1ee8a8df1
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-04-22 10:54:29 -0300

    test draft review

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -39,7 +39,7 @@ module Api
     private
 
     def incorrect_user_error
-      { json: { errors: 'Usuario incorrecto' }, status: :unauthorized }
+      { json: { errors: 'Usuario incorrecto test draft' }, status: :unauthorized }
     end
 
     def comment_errors
