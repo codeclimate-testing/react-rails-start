0501bb7c724f1cf786f2813f298a423715b402e8
dantevvp <dantevvp@gmail.com>
2022-05-24 16:12:30 -0300

    change config

---
+++ .eslintrc.js
@@ -0,0 +1,100 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  globals: {
+    CKEDITOR: true,
+    d3: true,
+    moment: true,
+    radialProgress: true,
+    VHL: true,
+    VISTA_ONLINE_LEARNING: true,
+  },
+  rules: {
+    'max-len': ['error', 100],
+    'object-curly-spacing': ['error', 'always', { 'objectsInObjects': false }],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'never',
+      },
+    ],
+    'indent': [
+      'error', 2, {
+        'CallExpression': {
+          'arguments': 1,
+        },
+      },
+    ],
+    'require-jsdoc': [
+      'warn',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+        },
+      },
+    ],
+    'quotes': [
+      'error', 'single', { 'avoidEscape': true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      globals: {
+        defineProps: true,
+        defineEmits: true,
+      },
+      extends: ['plugin:vue/recommended'],
+      globals: {
+        defineProps: true,
+        defineEmits: true,
+      },
+      plugins: ['vue'],
+      rules: {
+        'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
+        'vue/attribute-hyphenation': ['error', 'never'],
+        'vue/html-closing-bracket-newline': [
+          'error',
+          {
+            'singleline': 'never',
+            'multiline': 'never',
+          },
+        ],
+        'vue/max-attributes-per-line': [
+          'warn',
+          {
+            'singleline': 3,
+            'multiline': { 'max': 1, 'allowFirstLine': false },
+          },
+        ],
+        'indent': 'off',
+      },
+    },
+    {
+      files: ['**/*Spec.js', '**/*spec.js'],
+      env: {
+        jasmine: true,
+        jest: true,
+      },
+      rules: {
+        'require-jsdoc': 'off',
+      },
+    },
+  ],
+};
