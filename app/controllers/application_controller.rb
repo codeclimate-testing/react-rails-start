17cc9510eba3f749e872fe109536abc347a9d22c
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-05-30 14:38:09 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def set_csrf_cookie
-    puts "aaa test"
+    puts "aaa test 2"
     cookies['X-CSRF-Token'] = form_authenticity_token
   end
 end
