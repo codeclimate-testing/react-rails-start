40536b71883e8e00a85fc131ab8746d991d94a22
dantevvp <dantevvp@gmail.com>
2022-06-17 16:06:29 -0300

    aeiou

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import 'stylesheets/application'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const aeiou = 'aoiuer';
 // Support component names relative to this directory:
 const componentRequireContext = require.context('components', true)
 const ReactRailsUJS = require('react_ujs')
