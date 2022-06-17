00b4f92b7635f5e36de9678c339f7b14bcfca227
dantevvp <dantevvp@gmail.com>
2022-06-17 16:12:31 -0300

    hola

--- app/javascript/components/button/NavButton.js
+++ app/javascript/components/button/NavButton.js
@@ -5,6 +5,7 @@ const NavButton = (props) => {
   const { key, href, children, ...rest } = props
   const classes = 'p-3 hover:bg-green-900 transition-colors duration-300 rounded-lg'
   const unused_variable = 'holaaaaaaa'
+  const rompete_ya = '!!!!!'
   const otra = 'otraa'
   return href
     ? (
