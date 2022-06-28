e394161fbb7a5236b2eb4cd334030c5f040e7ef8
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-06-28 17:05:27 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def set_csrf_cookie
     cookies['X-CSRF-Token'] = form_authenticity_token
-    puts "test skipping draft"
+    puts "test skipping draft 2"
   end
 end
