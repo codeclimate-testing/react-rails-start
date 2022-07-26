6f250ad30ef5065bd429c3e048c4070454a36829
Camillo Facello <52419977+camillof@users.noreply.github.com>
2022-07-26 17:34:14 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def set_csrf_cookie
     cookies['X-CSRF-Token'] = form_authenticity_token
-    puts "Remove this someday"
+    puts "Remove this someday. Is today that day?"
   end
 end
